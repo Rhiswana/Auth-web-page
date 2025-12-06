4c5e347db3f4e016dcdd96534fecc457ba01d885
Rhiswana <rhiswanabegam@gmail.com>
2025-12-07 01:38:46 +0530

    frontend folders added

--- Auth_Project/Frontend/register.js
+++ Auth_Project/Frontend/register.js
@@ -3,11 +3,11 @@
     const email = document.getElementById("email").value;
     const password = document.getElementById("Password").value;
 
-    fetch("https://auth-web-page.onrender.com/register",({
+    fetch("https://auth-web-page.onrender.com/register",{
         method:"POST",
         headers:{"Content-Type":"application/json"},
         body:JSON.stringify({username,email,password})
-    }))
+    })
     .then(res=>res.text())
     .then(data=>{
          document.getElementById("errorMsg").innerText=data;
